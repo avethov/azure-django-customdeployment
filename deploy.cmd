bbd3c4d5247e3404c09b60916d1eb598e7a95cca
avethov <vethov.alexey@gmail.com>
2016-12-01 18:35:16 +0300

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -84,7 +84,7 @@ pushd "%DEPLOYMENT_TARGET%"
 
 :: 3. Install packages
 echo Pip install requirements.
-D:\home\site\wwwroot\env\Scripts\python.exe -m pip install --upgrade -r requirements.txt
+env\Scripts\python.exe -m pip install --upgrade -r requirements.txt
 IF !ERRORLEVEL! NEQ 0 goto error
 
 popd
